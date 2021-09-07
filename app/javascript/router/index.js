e09777a72783362bcd44b0a5271272a99206e979
noguchi <02125011a@gmail.com>
2021-09-07 22:24:46 +0900

    Update: router/index.jsにvue-routerの設定を記入

---
+++ app/javascript/router/index.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Top from "../pages/top";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: Top,
+  },
+];
+
+export default new VueRouter({ routes });
