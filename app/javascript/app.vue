6eaae694959855a346e582e2c2714e67fd380845
noguchi <02125011a@gmail.com>
2021-09-08 00:27:30 +0900

    Update: headerとfooterをコンポーネントとして設定

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,3 +1,21 @@
 <template>
-  <div><router-view /></div>
+  <div>
+    <div class="d-flex flex-column min-vh-100">
+      <Header />
+      <router-view />
+      <Footer />
+    </div>
+  </div>
 </template>
+
+<script>
+import Header from "./components/TheHeader.vue";
+import Footer from "./components/TheFooter.vue";
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
