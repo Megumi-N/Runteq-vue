02eba966def9a2a19a9d914ced721ed4b0f14054
noguchi <02125011a@gmail.com>
2021-09-07 22:49:35 +0900

    Add: pages/task/index.vueを作成し、topページに戻るリンクを作成

---
+++ app/javascript/pages/task/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-link to="/" type="button" class="btn btn-dark mt-5">
+    戻る
+  </router-link>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
