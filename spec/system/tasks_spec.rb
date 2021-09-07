017d61eedb47d4e23aca50867fe6910c777bcc65
noguchi <02125011a@gmail.com>
2021-09-07 23:01:06 +0900

    Update: taskページとtopページの移動に関するRSpecを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,7 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "タスク管理", type: :system do
   it 'トップページが表示されている' do
+    visit root_path # rootパスへ移動
+    expect(page).to have_content('はじめる'), 'はじめるボタンがトップページに表示されていません' # ページ中に'はじめる'という言葉があるか確認
+  end
+
+  it 'タスクページに戻るボタンが表示されている' do
+    visit '/tasks'
+    expect(page).to have_content('戻る'), 'タスクページに戻るボタンが表示されていません'
+  end
+
+  it 'トップページからタスクページに遷移できる' do
     visit root_path
-    expect(page).to have_content('はじめる'), 'はじめるボタンがトップページに表示されていません'
+    click_link 'はじめる'
+    expect(current_path).to eq('/tasks'), 'タスクページに遷移できていません'
+  end
+
+  it 'タスクページからトップページに遷移できる' do
+    visit '/tasks'
+    click_link '戻る'
+    expect(current_path).to eq('/'), 'トップページに遷移できていません'
   end
 end
