798b9df2de1fd1555d117973662336959ddfe3e7
noguchi <02125011a@gmail.com>
2021-09-07 21:45:44 +0900

    Update: templateの中身を削除し、<router-view></router-view>を記載

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,4 @@
-<template>
-  <div id="app" class="d-flex flex-column min-vh-100">
-    <header class="mb-auto">
-      <nav class="navbar navbar-dark bg-dark">
-        <span class="navbar-brand mb-0 h1">{{ title }}</span>
-      </nav>
-    </header>
-    <div class="text-center">
-      <h3>タスクを管理しよう！</h3>
-      <div class="mt-4">
-        生活や仕事に関するタスクを見える化して抜け漏れを防ぎましょう。
-      </div>
-      <button type="button" class="btn btn-dark mt-5">はじめる</button>
-    </div>
-    <footer class="text-center mt-auto">
-      <small>Copyright &copy; 2020. RUNTEQ</small>
-    </footer>
-  </div>
-</template>
+<template> <router-view></router-view> </template>
 
 <script>
 export default {
