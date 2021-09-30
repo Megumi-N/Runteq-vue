b5c96781bae15f3e41c2c446d83588563a904d40
noguchi <02125011a@gmail.com>
2021-09-30 18:35:39 +0900

    update: axiosを追加

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import App from "../app.vue";
 import "bootstrap/dist/css/bootstrap.css";
 import router from "../router";
+import axios from "../plugins/axios";
 
 Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
 
 document.addEventListener("DOMContentLoaded", () => {
   const app = new Vue({
